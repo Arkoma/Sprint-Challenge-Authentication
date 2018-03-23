f0f159e2192735007f23e8b46cb47c3b64d338b3
Aaron Burk <aarondburk@gmail.com>
2018-03-23 12:56:02 -0500

    set up jokes.js component

---
+++ jokepage/src/components/jokes.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getJokes } from '../actions';
+
+class Users extends Component {
+  componentDidMount() {
+    this.props.getJokes();
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.jokes.map((joke, i) => {
+          return (
+            <div className="joke-container"> 
+              <li key={i}>{joke.type}</li>
+              <li>{joke.setup}</li>
+              <li>{joke.punchline}</li>
+            </div>);
+        })}
+      </ul>
+    );
+  }
+}
+
+
+const mapStateToProps = state => {
+  return {
+    jokes: state.jokes
+  };
+};
+
+export default connect(mapStateToProps, { getJokes })(Jokes);
