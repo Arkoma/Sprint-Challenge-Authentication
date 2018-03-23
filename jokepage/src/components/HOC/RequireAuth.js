0d082285167a7f5d1789242ba60630e0184c546e
Aaron Burk <aarondburk@gmail.com>
2018-03-23 12:38:46 -0500

    set up RequireAuth.js HOC

---
+++ jokepage/src/components/HOC/RequireAuth.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default ComposedComponent => {
+  class RequireAuthentication extends Component {
+    componentDidMount() {
+      console.log('RequireAuthentication Props: ', this.props);
+      if (!this.props.authenticated) {
+        this.props.history.push('/signin');
+      }
+    }
+    
+    render() {
+      return !this.props.authenticated ? null : <ComposedComponent />
+    }
+  }
+
+  const mapStateToProps = state => {
+    return {
+      authenticated: state.auth.authenticated
+    };
+  };
+
+  return connect(mapStateToProps)(RequireAuthentication);
+};
