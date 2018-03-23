cbf4d778c1c6d2f11bbac463ce160973bc5cf947
Aaron Burk <aarondburk@gmail.com>
2018-03-23 12:25:26 -0500

    add action/index.js

---
+++ jokepage/src/actions/index.js
@@ -0,0 +1,77 @@
+import axios from 'axios';axios.defaults.withCredentials = true;
+const ROOT_URL = 'http://localhost:5000';
+
+export const USER_REGISTERED = 'USER_REGISTERED';
+export const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
+export const USER_UNAUTHENTICATED = 'USER_UNAUTHENTICATED';
+export const AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR';
+export const GET_JOKES = 'GET_JOKES';
+export const CHECK_IF_AUTHENTICATED = 'CHECK_IF_AUTHENTICATED';
+
+export const authError = error => {
+  return {
+    type: AUTHENTICATION_ERROR,
+    payload: error
+  };
+};
+
+export const register = (username, password, confirmPassword, history) => {
+  return dispatch => {
+    if (password !== confirmPassword) {
+      dispatch(authError('Passwords do not match'));
+      return;
+    }
+    axios
+      .post(`${ROOT_URL}/api/users`, { username, password })
+      .then(() => {
+        dispatch({
+          type: USER_REGISTERED
+        });
+        history.push('/signin');
+      })
+      .catch(() => {
+        dispatch(authError('Failed to register user'));
+      });
+  };
+};
+
+export const login = (username, password, history) => {
+  return dispatch => {
+    axios
+      .post(`${ROOT_URL}/api/login`, { username, password })
+      .then((res) => {
+        dispatch({
+          type: USER_AUTHENTICATED
+        });
+        history.push('/users');
+        localStorage.setItem('token', res.data.token);
+      })
+      .catch(() => {
+        dispatch(authError('Incorrect username/password combo'));
+      });
+  };
+};
+
+export const logout = () => {
+  localStorage.removeItem('token');
+  console.log('Logout token line 61: ', localStorage.getItem('token'));
+  return { type: USER_UNAUTHENTICATED };
+};
+
+export const getJokes = () => {
+  const token = localStorage.getItem('token');
+  console.log('Get jokes token line 79: ', token);
+  return dispatch => {
+    axios
+      .get(`${ROOT_URL}/api/jokes`, { headers: { authorization: token } })
+      .then(response => {
+        dispatch({
+          type: GET_JOKES,
+          payload: response.data
+        });
+      })
+      .catch(() => {
+        dispatch(authError('Failed to fetch jokes'));
+      });
+  };
+};
